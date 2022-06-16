4a252a00c9888dca92d5d39d0c3772ceeff648fe
Meen Tycoon <meenphilip@gmail.com>
2022-06-16 16:44:53 +0300

    Added loadEventListeners to loads all events, added addTask function to add task

--- app.js
+++ app.js
@@ -10,3 +10,46 @@ const clearBtn = document.querySelector("#clear-tasks");
 const taskInput = document.getElementById("task");
 //get the filter input
 const filterTask = document.querySelector("#task");
+
+//load all eventlisteners
+const loadEventListeners = () => {
+  // Add task Event
+  form.addEventListener("submit", addTask);
+};
+
+// Add Task
+const addTask = (e) => {
+  //No submit of empty value
+  if (taskInput.value === "") {
+    alert("Add a task");
+  }
+
+  //create li element
+  const li = document.createElement("li");
+  // Add a class
+  li.className = "list-group-item";
+  // create Text node & append to li
+  const text = document.createTextNode(taskInput.value);
+  // append text node to li
+  li.appendChild(text);
+  // create link element
+  const link = document.createElement("a");
+  // Add a class
+  link.className = "float-end text-danger";
+  // add html icon
+  link.innerHTML = '<i class="fas fa-times-circle"></i>';
+  //append link to li
+  li.appendChild(link);
+
+  // Append li to Ul
+  taskList.appendChild(li);
+
+  // clear the input
+  taskInput.value = "";
+
+  // prevent default element behaviour
+  e.preventDefault();
+};
+
+// FUNCTION TO LOAD EVENTLISTENERS
+loadEventListeners();
