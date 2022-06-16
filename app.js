52f143750464dc4d3f91754cec7a01e0ae8fc0e8
Meen Tycoon <meenphilip@gmail.com>
2022-06-16 15:50:56 +0300

    Added UI variables

---
+++ app.js
@@ -0,0 +1,12 @@
+// SELECTING ITEMS FROM DOM
+
+//get the form
+const form = document.getElementById("task-form");
+//get the task list ul
+const taskList = document.querySelector(".list-group");
+//get the clear button
+const clearBtn = document.querySelector("#clear-tasks");
+//get the task input
+const taskInput = document.getElementById("task");
+//get the filter input
+const filterTask = document.querySelector("#task");
